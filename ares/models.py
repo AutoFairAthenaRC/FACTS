69d3b1ef0ae98f9cae7547412f2b4daa22b6423d
phantom-duck <37215464+phantom-duck@users.noreply.github.com>
2023-01-03 03:38:50 +0200

    Minor changes

--- ares/models.py
+++ ares/models.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn as sk
-from typing import List
+from typing import List, Optional
 
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
@@ -12,11 +12,11 @@ from omnixai.preprocessing.tabular import TabularTransform
 class customXGB:
     def __init__(self, n_estimators=300, max_depth=5):
         self.clf = XGBClassifier(n_estimators=n_estimators, max_depth=max_depth)
-        self.transformer = None
-        self.cate_columns = None
-        self.target_column = None
+        self.transformer = TabularTransform()
+        self.cate_columns = []
+        self.target_column = "label"
 
-    def fit(self, data: pd.DataFrame, cate_columns: List[str] = None, target_column: str = "label"):
+    def fit(self, data: pd.DataFrame, cate_columns: Optional[List[str]] = None, target_column: str = "label"):
         self.cate_columns = cate_columns if cate_columns is not None else []
         self.target_column = target_column
         tabular_data = Tabular(
@@ -24,7 +24,7 @@ class customXGB:
             categorical_columns=self.cate_columns,
             target_column=self.target_column
         )
-        self.transformer = TabularTransform().fit(tabular_data)
+        self.transformer.fit(tabular_data)
         x = self.transformer.transform(tabular_data)
         train, test, train_labels, test_labels = train_test_split(x[:, :-1], x[:, -1], train_size=.8)
         self.clf.fit(train, train_labels)
