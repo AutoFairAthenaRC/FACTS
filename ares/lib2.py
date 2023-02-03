888596bda1b8458b13d59dba7c2c0b249b9e4ff5
phantom-duck <37215464+phantom-duck@users.noreply.github.com>
2023-02-03 23:54:10 +0200

    started experimenting with "total recourse costs" for ifs

--- ares/lib2.py
+++ ares/lib2.py
@@ -11,7 +11,7 @@ from metrics import incorrectRecoursesSingle
 
 ## Re-exporting
 from optimization import optimize_vanilla
-from predicate import Predicate, recIsValid
+from predicate import Predicate, recIsValid, featureChangePred
 ## Re-exporting
 
 
@@ -149,3 +149,47 @@ def rules2rulesbyif(rules: List[Tuple[Predicate, Predicate, Dict[str, float], Di
     return rules_by_if
 
 
+
+
+
+
+def calculate_if_group_cost(
+    ifclause: Predicate,
+    thenclauses: List[Tuple[Predicate, float, float]],
+    params: ParameterProxy = ParameterProxy()
+) -> float:
+    return float(np.mean([cor * featureChangePred(ifclause, thenclause, params=params) for thenclause, _, cor in thenclauses]))
+
+def calculate_if_group_costs(
+    ifclause: Predicate,
+    thenclauses: Dict[str, List[Tuple[Predicate, float, float]]],
+    params: ParameterProxy = ParameterProxy()
+) -> Dict[str, float]:
+    return {sg: calculate_if_group_cost(ifclause, thens, params) for sg, thens in thenclauses.items()}
+
+def calculate_cost_difference_2groups(
+    ifclause: Predicate,
+    thenclauses: Dict[str, List[Tuple[Predicate, float, float]]],
+    params: ParameterProxy = ParameterProxy()
+) -> float:
+    group_costs = list(calculate_if_group_costs(ifclause, thenclauses, params).values())
+    return abs(group_costs[0] - group_costs[1])
+
+def sort_triples_by_costdiff_2groups(
+    rulesbyif: Dict[Predicate, Dict[str, List[Tuple[Predicate, float, float]]]],
+    params: ParameterProxy = ParameterProxy()
+) -> Dict[Predicate, Dict[str, List[Tuple[Predicate, float, float]]]]:
+    def apply_calc(ifthens):
+        return calculate_cost_difference_2groups(ifthens[0], ifthens[1], params)
+    ret = sorted(rulesbyif.items(), key=apply_calc, reverse=True)
+    return dict(ret)
+
+def construct_feature_cost_dict(num_cols: List[str], cate_cols: List[str]):
+    def feature_change_cate(v1, v2):
+        return 0 if v1 == v2 else 1
+    def feature_change_num(v1, v2):
+        return abs(v1 - v2)
+    
+    ret_cate = {col: feature_change_cate for col in cate_cols}
+    ret_num = {col: feature_change_num for col in num_cols}
+    return {**ret_cate, **ret_num}
