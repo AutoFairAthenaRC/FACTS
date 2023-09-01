f240aa7f188e56de11bdfafa3a48b7e4f94d372f
phantom-duck <37215464+phantom-duck@users.noreply.github.com>
2023-09-01 11:39:51 +0300

    minor refactoring of clean and parameters

--- facts/facts/clean.py
+++ facts/facts/clean.py
@@ -2,6 +2,48 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 
+def clean_adult(X: DataFrame) -> DataFrame:
+    X = X.drop(columns=["fnlwgt", "education"])
+    cols = list(X.columns)
+    X[cols] = X[cols].replace([" ?"], np.nan)
+    X = X.dropna()
+    X["relationship"] = X["relationship"].replace([" Husband", " Wife"], " Married")
+    X["hours-per-week"] = pd.cut(
+        x=X["hours-per-week"],
+        bins=[0.9, 25, 39, 40, 55, 100],
+        labels=["PartTime", "MidTime", "FullTime", "OverTime", "BrainDrain"],
+    )
+    X.age = pd.qcut(X.age, q=5)
+    X["income"] = np.where((X["income"] == " <=50K"), 0, 1)
+
+    return X
+
+def clean_ssl(X: DataFrame) -> DataFrame:
+    X["SSL SCORE"] = np.where((X["SSL SCORE"] >= 345), 0, 1)
+    X = X.replace("WBH", "BLK")
+    X = X.replace("WWH", "WHI")
+    X = X.replace("U", np.nan)
+    X = X.replace("X", np.nan)
+    X = X.dropna()
+    X = X[(X["RACE CODE CD"] != "I") & (X["RACE CODE CD"] != "API")]
+    X["PREDICTOR RAT TREND IN CRIMINAL ACTIVITY"] = pd.qcut(
+        X["PREDICTOR RAT TREND IN CRIMINAL ACTIVITY"], q=6
+    )
+    X["PREDICTOR RAT AGE AT LATEST ARREST"] = X[
+        "PREDICTOR RAT AGE AT LATEST ARREST"
+    ].replace("less than 20", "10-20")
+
+    return X
+
+def clean_compas(X: DataFrame) -> DataFrame:
+    X = X.reset_index(drop=True)
+    X = X.drop(columns=["age", "c_charge_desc"])
+    X["priors_count"] = pd.cut(X["priors_count"], [-0.1, 1, 5, 10, 15, 38])
+    X.target.replace("Recidivated", 0, inplace=True)
+    X.target.replace("Survived", 1, inplace=True)
+    X["age_cat"].replace("Less than 25", "10-25", inplace=True)
+
+    return X
 
 def clean_dataset(X: DataFrame, dataset: str) -> DataFrame:
     """Cleans and modifies the input DataFrame based on the specified dataset types.
@@ -20,40 +62,10 @@ def clean_dataset(X: DataFrame, dataset: str) -> DataFrame:
     Examples:
         >>> df = clean_dataset(X, "adult")
     """
-
     if dataset == "adult":
-        X = X.drop(columns=["fnlwgt", "education"])
-        cols = list(X.columns)
-        X[cols] = X[cols].replace([" ?"], np.nan)
-        X = X.dropna()
-        X["relationship"] = X["relationship"].replace([" Husband", " Wife"], " Married")
-        X["hours-per-week"] = pd.cut(
-            x=X["hours-per-week"],
-            bins=[0.9, 25, 39, 40, 55, 100],
-            labels=["PartTime", "MidTime", "FullTime", "OverTime", "BrainDrain"],
-        )
-        X.age = pd.qcut(X.age, q=5)
-        X["income"] = np.where((X["income"] == " <=50K"), 0, 1)
+        X = clean_adult(X)
     elif dataset == "SSL":
-        X["SSL SCORE"] = np.where((X["SSL SCORE"] >= 345), 0, 1)
-        X = X.replace("WBH", "BLK")
-        X = X.replace("WWH", "WHI")
-        X = X.replace("U", np.nan)
-        X = X.replace("X", np.nan)
-        X = X.dropna()
-        X = X[(X["RACE CODE CD"] != "I") & (X["RACE CODE CD"] != "API")]
-        X["PREDICTOR RAT TREND IN CRIMINAL ACTIVITY"] = pd.qcut(
-            X["PREDICTOR RAT TREND IN CRIMINAL ACTIVITY"], q=6
-        )
-        X["PREDICTOR RAT AGE AT LATEST ARREST"] = X[
-            "PREDICTOR RAT AGE AT LATEST ARREST"
-        ].replace("less than 20", "10-20")
+        X = clean_ssl(X)
     elif dataset == "compas":
-        X = X.reset_index(drop=True)
-        X = X.drop(columns=["age", "c_charge_desc"])
-        X["priors_count"] = pd.cut(X["priors_count"], [-0.1, 1, 5, 10, 15, 38])
-        X.target.replace("Recidivated", 0, inplace=True)
-        X.target.replace("Survived", 1, inplace=True)
-        X["age_cat"].replace("Less than 25", "10-25", inplace=True)
-
+        X = clean_compas(X)
     return X
